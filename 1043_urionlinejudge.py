38f095b709bc5fe8b2b04df7507544236085e101
EwertonWeb <ewerton.rl@hotmail.com>
2019-12-17 02:10:20 -0300

    1043_Triângulo Solved

---
+++ 1043_urionlinejudge.py
@@ -0,0 +1,7 @@
+a, b, c = [float(x) for x in input().split(' ')]
+
+
+if abs(b - c) < a < (b + c) and (a - c) < b < (a + c) and (a - b) < c < (a + b):
+    print('Perimetro = {:.1f}'.format(a + b + c))
+else:
+    print('Area = {:.1f}'.format(((a + b) / 2) * c))
