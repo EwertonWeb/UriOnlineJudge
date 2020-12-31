f96a12b9ea39f712eb425b98810ecd5b7513c358
EwertonWeb <ewerton.rl@hotmail.com>
2020-12-31 20:45:51 -0300

    solved sequencia logica

---
+++ 1144_urionlinejudge.py
@@ -0,0 +1,8 @@
+NUMERO = int(input())
+
+x = 1
+
+while x <= NUMERO :
+    print(str(int(x))+" "+str(int(x*x))+" "+str(int(x*x*x)))
+    print(str(int(x))+" "+str(int(x*x+1))+" "+str(int(x*x*x+1)))
+    x += 1
